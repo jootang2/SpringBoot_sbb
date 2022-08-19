f936ed42cbff5a8d5a62b5f5f407ed73863cba7d
jootang2 <joohwan.song15b@gmail.com>
2022-08-19 11:41:30 +0900

    2-05 리포지터리

---
+++ src/main/java/com/mysite/sbb/mysite/AnswerRepository.java
@@ -0,0 +1,6 @@
+package com.mysite.sbb.mysite;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+}
--- src/test/java/com/mysite/sbb/mysite/MysiteApplicationTests.java
+++ src/test/java/com/mysite/sbb/mysite/MysiteApplicationTests.java
@@ -1,13 +1,26 @@
 package com.mysite.sbb.mysite;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class MysiteApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	void testJpa(){
+		Optional<Question> oq = questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 테스트 질문 입니다.");
+		questionRepository.save(q);
 	}
 
 }
